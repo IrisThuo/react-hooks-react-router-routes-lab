fd27bb62b149080edb643708ef95ccbdd73960d6
Iris <iris.thuo@student.moringaschool.com>
2023-01-22 22:27:29 +0300

    Completed Assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+    {/*{code here}*/}
+    <h1>Actors Page</h1>
+    {actors.map((actor,index)=>{
+        return(
+          <div key={index}>
+            <h2>Name:{actor.name}</h2>
+            <p>Movies:</p>
+            <ul>
+              {actor.movies.map((movie,newIndex)=>{
+                return(
+                  <li key={newIndex}>{movie}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>);
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+    {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie ,index)=>{
+      return(
+        <div key={index}>
+          <h2>Title:{movie.title}</h2>
+          <p>Time:{movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {movie.genres.map((genre,newIndex)=>{
+              return(
+                <li key={newIndex}>{genre}</li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })}
+    </div>);
 }
 
 export default Movies;
